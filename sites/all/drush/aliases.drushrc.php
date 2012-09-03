2c1be3cf53cb7cb52562d4d55bb4971ed080ae12
Edouard Cunibil <admin@duael.fr>
2012-09-03 15:08:00 +0200

    up drush aliases

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -1,21 +1,44 @@
 <?php
 
-$base_domain = '.gn-capes-et-crocs.fr';
-
-$aliases['local'] = array(
-  'uri' => 'local' . base_domain,
-  'root' => 'D:\wamp\www\gncec',
+// Sites under this core
+$sites = array(
+  array(
+    'base_domain' => 'gn-capes-et-crocs',
+    'prefix' => '',
+  ),
 );
-$aliases['dev'] = $aliases['local'];
 
-$aliases['stage'] = array(
-  'uri' => 'preprod' . base_domain,
-  'root' => '/home/gn-capes-et-crocs.fr/sd/preprod',
-);
-$aliases['preprod'] = $aliases['stage'];
+// OS dependent base dir
+$local_base_dir = dirname(dirname(dirname(dirname(__FILE__))));
 
-$aliases['live'] = array(
-  'uri' => 'www' . base_domain,
-  'root' => '/home/gn-capes-et-crocs.fr/www',
-);
-$aliases['prod'] = $aliases['live'];
+foreach ($sites as $site) {
+  $bd = $site['base_domain'];
+  $pre = $site['prefix'];
+  $pre = !empty($pre) ? $pre . '_' : $pre;
+  
+  // --- LOCAL -----------------------------------------------------------------
+  $aliases[$pre . 'local'] = array(
+    'uri' => 'local.' . $bd,
+    'root' => $local_base_dir,
+  );
+  
+  // --- DEV -------------------------------------------------------------------
+  $aliases[$pre . 'dev'] = array(
+    'uri' => 'dev.' . $bd,
+    'root' => '/home/' . $bd . '/sd/dev',
+  );
+  
+  // --- STAGE -----------------------------------------------------------------
+  $aliases[$pre . 'stage'] = array(
+    'uri' => 'preprod.' . $bd,
+    'root' => '/home/' . $bd . '/sd/preprod',
+  );
+  $aliases[$pre . 'preprod'] = $aliases[$pre . 'stage'];
+  
+  // --- LIVE ------------------------------------------------------------------
+  $aliases[$pre . 'live'] = array(
+    'uri' => 'www.' . $bd,
+    'root' => '/home/' . $bd . '/www',
+  );
+  $aliases[$pre . 'prod'] = $aliases[$pre . 'live'];
+}
