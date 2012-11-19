5220c9ee581e2a9dd021c1444aadd43eb5767bf8
Edouard Cunibil <admin@duael.fr>
2012-11-19 16:54:26 +0100

    up module global_filter

--- sites/all/modules/global_filter/global_filter.js
+++ sites/all/modules/global_filter/global_filter.js
@@ -1,18 +1,39 @@
 
 (function($) {
-  Drupal.behaviors.GlobalFilterSubmitOnChange = {
+
+  Drupal.behaviors.GlobalFilterSubmit = {
     attach: function(context, setting) {
       var filter_names = [setting.global_filter_1, setting.global_filter_2, setting.global_filter_3, setting.global_filter_4, setting.global_filter_5];
       // This may be called up to five times in one request, catering for up to
       // five global filter blocks on the same page.
       for (var i = 0; i < 5; i++) {
         if (filter_names[i] != undefined) {
-          var selector = '.' + filter_names[i];
-          //alert('selector ' + selector);
-          $(selector).change(function() {
-            // Find enclosing form and auto-submit as soon as a new value is selected.
-            for (var element = $(this).parent(); !element.is('form'); element = element.parent()) {}
-            element.submit();
+          var claz = '.' + filter_names[i][0];
+          var confirm_question = filter_names[i][1];
+          var auto_submit = filter_names[i][2];
+          var selectors = [claz, claz+' select', claz+' input:*[checked]'];
+          var selection;
+          for (var s=0; s < selectors.length; s++) {
+            if (selection = $(selectors[s]).val()) {
+              var selector = selectors[s];
+              break;
+            }
+          }
+          //alert(selector + ': ' + selection + ', ' + prompt_confirm + ', ' + auto_submit);
+          $(s == 1 ? selector : claz).change(function() {
+            if (confirm_question) {
+              if (!confirm(Drupal.t(confirm_question))) {
+                // Restore selection
+                $(this).val(selection);
+                return;
+              }
+              auto_submit = true;
+            }
+            if (auto_submit) {
+              // Find enclosing form and auto-submit as soon as a new value is selected.
+              for (var element = $(this).parent(); !element.is('form'); element = element.parent()) {}
+              element.submit();
+            }
           });
         }
       }
