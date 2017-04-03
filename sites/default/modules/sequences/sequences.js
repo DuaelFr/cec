7db9511faf4c1a19d6543134a0e30a5fe7c0206a
Edouard Cunibil <edouard@happyculture.coop>
2017-04-04 01:16:23 +0200

    Improve sequences time widget.

--- sites/default/modules/sequences/sequences.js
+++ sites/default/modules/sequences/sequences.js
@@ -3,10 +3,8 @@
   Drupal.behaviors.Sequences = {
     'attach': function(context) {
       var form = $('#sequence-node-form', context);
-      $('.form-type-date-select .date-month :input', form).attr('disabled', true);
       $('.form-type-date-select .date-year :input', form).attr('disabled', true);
       form.submit(function() {
-        $('.form-type-date-select .date-month :input',form).attr('disabled', false);
         $('.form-type-date-select .date-year :input', form).attr('disabled', false);
       });
     }
